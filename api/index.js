618525534e80e41731db57dc44c39f986b453403
Office <twinklevpopat20@gmail.com>
2024-03-03 23:07:30 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect("mongodb+srv://twinkle:twinkle@snapshelter.5ldxzpf.mongodb.net/?retryWrites=true&w=majority&appName=SnapShelter").then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Error:', error);
+});
 const app = express();
 
 app.listen(3000, () => {
